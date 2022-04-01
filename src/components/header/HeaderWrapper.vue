843fc75324c2c64402d0ba430a027507bcc5c71f
Donatella7 <campaniellodonatella@gmail.com>
2022-04-01 12:29:39 +0200

    header style

--- src/components/header/HeaderWrapper.vue
+++ src/components/header/HeaderWrapper.vue
@@ -1,9 +1,13 @@
 <template>
     <header>
         <section class="container-fluid">
-            <div class="row">
-                <div class="col-12">
+            <div class="row align-items-center justify-content-between px-5 py-2">
+                <div class="col-6">
                     <h1 class="text-danger">Boolflix</h1>
+                </div>
+                <div class="col-6 d-flex align-items-center justify-content-end">
+                    <input class="me-2" type="text">
+                    <button class="btn btn-danger text-white">Cerca!</button>
                 </div>
             </div>
         </section>
@@ -17,5 +21,8 @@ name:"HeaderWrapper"
 </script>
 
 <style lang="scss" scoped>
-
+header{
+    height: 15vh;
+    background-color: black;
+}
 </style>
