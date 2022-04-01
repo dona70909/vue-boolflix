9086b90624d030fcb51b72df3aa930b3f0277a7c
Donatella7 <campaniellodonatella@gmail.com>
2022-04-01 12:21:17 +0200

    start boot and axios

---
+++ src/components/header/HeaderWrapper.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+        <section class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <h1 class="text-danger">Boolflix</h1>
+                </div>
+            </div>
+        </section>
+    </header>
+</template>
+
+<script>
+export default {
+name:"HeaderWrapper"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
