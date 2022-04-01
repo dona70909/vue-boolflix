5db2edc5cad1133ae9eb0376bc231f431a6d99c2
Donatella7 <campaniellodonatella@gmail.com>
2022-04-01 18:31:45 +0200

    ok

--- src/components/main/FilmCard.vue
+++ src/components/main/FilmCard.vue
@@ -2,8 +2,8 @@
     <div class="card" style="width: 18rem;">
         <div class="card-body">
             <h5 class="card-title">{{title}}</h5>
-            <country-flag :country='language' size='small'/>
-            <p class="card-text"> {{originalTitle}} - {{language}} -  {{vote}} </p>
+            <country-flag v-if="language.length != 0" :country='language' size='small'/>
+            <p class="card-text"> {{originalTitle}} - {{vote}} </p>
         </div>
     </div>
 </template>
