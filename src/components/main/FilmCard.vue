a9a648079da6972dbbbf798e03cbab088ca6d995
Donatella7 <campaniellodonatella@gmail.com>
2022-04-05 12:59:20 +0200

    comment

--- src/components/main/FilmCard.vue
+++ src/components/main/FilmCard.vue
@@ -35,8 +35,10 @@
             <p v-show="overview != '' " class="overview-text mb-1">{{overview}}</p>
 
             <p class="text-danger">Vote:</p>
-            <i v-for="(star,index) in starVote(vote)" :key="index" class="bi bi-star-fill"></i>
-            <i  v-for="(star,index) in starVote(5 - vote)" :key="index + 'zero'"    class="bi bi-star"></i>  
+            <!-- <i v-show="vote != 0"  v-for="(star,index) in starVote(vote)" :key="index + 'full'" class="bi bi-star-fill"></i>
+            <i v-show="vote != null"  v-for="(star,index) in starVote(5 - vote)" :key="index + 'empty'" class="bi bi-star"></i>  
+            <i v-show="vote == 0" v-for="(star,index) in starVote(5)" :key="index + 'zero'" class="bi bi-star"></i>  
+            -->
             <div>
                 <p class="text-danger mb-0">Actors/Actress</p>
                 <span  class="text-actors" v-for="(actor,indexAct) in castMovie" :key="indexAct + 'name' ">{{actor + " | "}}</span>
@@ -137,6 +139,7 @@ export default {
         
     }, 
 
+    //prova computed return castMovie list
     mounted(){
         console.warn(this.id);
         this.getCast();
