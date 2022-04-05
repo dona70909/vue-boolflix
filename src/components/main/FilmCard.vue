27cb79db2f1c29dcd5e5811b96ac4411d5b3b169
Donatella7 <campaniellodonatella@gmail.com>
2022-04-05 15:14:20 +0200

    style card

--- src/components/main/FilmCard.vue
+++ src/components/main/FilmCard.vue
@@ -1,6 +1,6 @@
 <template>
     <!-- col -->
-    <div class="card position-relative">
+    <div class="card">
         <!-- poster null and backdrop ok -->
         <img v-show="poster != null" :class="((poster == null) && (backdrop != null))? 'd-none' : '' " class="card-img-top poster-img" :src="'http://image.tmdb.org/t/p/w500/' + poster"  :alt="title">
         <div class="poster-img-empty " v-if="poster == null && backdrop != null">
@@ -17,7 +17,7 @@
         </div>
         <div class="back-img-empty" v-if="(backdrop == null) && (poster == null)"></div>
 
-        <div class="card-body p-0 position-absolute">
+        <div class="card-body">
 
             <p class="text-danger">Titolo:</p>
             <p class="card-title">{{title}}</p>
@@ -142,106 +142,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.card{
-    height: 300px; 
-
-    box-shadow: 2px 2px 2px rgb(14, 13, 13);
-    border: none;
-    img{
-        width:100%;
-        height: 300px;
-
-        object-fit: center;
-    }
-    .poster-img-empty{
-        /* only when poster image == null */
-        width:100%;
-        height: 300px;
-
-        background: rgb(162, 20, 20);
-        font-weight: 900;
-        text-shadow: 2px 2px 10px black;
-        text-transform: uppercase;
-        display:flex; 
-        justify-content: center; 
-        align-items:center;
-    }
-    .back-img-empty{
-        /* only backdrop == null  */
-        display: none;
-
-        width:100%;
-        height: 300px;
-
-        background: rgb(46, 44, 44);
-    }
-    .card-body{
-        /* text/body visible only in hover */
-        display: none;
-        top:.3rem;
-
-        height: 300px;
-
-        left: .5rem;
-        color:white;
-        text-shadow: 1px 1px 1px black;
-        text-transform: uppercase;
-        font-weight: 700;
-        p{
-            margin: 0;
-            padding:0;
-            font-size: .8rem;
-        }
-        .bi-star-fill{
-            color: rgb(220, 220, 23);
-            margin: 0 .1rem;
-        }
-
-        .overview-text{
-            font-size: .6rem;
-            height: 3.5rem;
-            overflow-y:auto;
-            padding: 0 .2rem;
-            scrollbar-color: #993338 #5f625f;
-            scrollbar-width: thin;
-        }
-
-        .text-actors{
-            font-size: .5rem;
-        }
-    }
-}
-
-// # d-none di base- cambia in hover 
-.back-img{
-    display: none;
-}
-
-// # hover and d-none del poster-poster-bg-text 
-.card:hover .poster-img{
-    display: none;
-}
-.card:hover .poster-img-empty{
-    display: none;
-    
-}
-
-.card:hover .text-poster-empty{
-    display: none;
-}
-
-// # hover backdropb visible 
-.card:hover .back-img{
-    display:block;
-    /* opacity: 0.8; */
-    filter: brightness(0.5);
-}
-.card:hover .back-img-empty{
-    display: block;
-}
-
-.card:hover .card-body{
-    display:block;
-}
+@import '@/assets/scss/style.scss';
 </style>
 
--- src/components/main/TvCard.vue
+++ src/components/main/TvCard.vue
@@ -34,7 +34,7 @@
             <p v-show="overview != '' " class="text-danger">Overview:</p>
             <p v-show="overview != '' " class="overview-text mb-1">{{overview}}</p>
 
-            <p v-if="vote != 0" class="text-danger">Vote:</p>
+            <p  class="text-danger">Vote:</p>
             <i  v-for="(star,index) in starVote(vote)" :key="index + 'full'" class="bi bi-star-fill"></i>
             <i  v-for="(starEmpty,index) in emptyStars(vote)" :key="index + 'empty'" class="bi bi-star"></i> 
             <div>
@@ -134,105 +134,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.card{
-    height: 300px; 
-
-    box-shadow: 2px 2px 2px rgb(14, 13, 13);
-    border: none;
-    img{
-        width:100%;
-        height: 300px;
-
-        object-fit: center;
-    }
-    .poster-img-empty{
-        /* only when poster image == null */
-        width:100%;
-        height: 300px;
-
-        background: rgb(162, 20, 20);
-        font-weight: 900;
-        text-shadow: 2px 2px 10px black;
-        text-transform: uppercase;
-        display:flex; 
-        justify-content: center; 
-        align-items:center;
-    }
-    .back-img-empty{
-        /* only backdrop == null  */
-        display: none;
-
-        width:100%;
-        height: 300px;
-
-        background: rgb(46, 44, 44);
-    }
-    .card-body{
-        /* text/body visible only in hover */
-        display: none;
-        top:.3rem;
-
-        height: 300px;
-
-        left: .5rem;
-        color:white;
-        text-shadow: 1px 1px 1px black;
-        text-transform: uppercase;
-        font-weight: 700;
-        p{
-            margin: 0;
-            padding:0;
-            font-size: .8rem;
-        }
-        .bi-star-fill{
-            color: rgb(220, 220, 23);
-            margin: 0 .1rem;
-        }
-
-        .overview-text{
-            font-size: .6rem;
-            height: 3.5rem;
-            overflow-y:auto;
-            padding: 0 .2rem;
-            scrollbar-color: #993338 #5f625f;
-            scrollbar-width: thin;
-        }
-
-        .text-actors{
-            font-size: .5rem;
-        }
-    }
-}
-
-// # d-none di base- cambia in hover 
-.back-img{
-    display: none;
-}
-
-// # hover and d-none del poster-poster-bg-text 
-.card:hover .poster-img{
-    display: none;
-}
-.card:hover .poster-img-empty{
-    display: none;
-    
-}
-
-.card:hover .text-poster-empty{
-    display: none;
-}
-
-// # hover backdropb visible 
-.card:hover .back-img{
-    display:block;
-    /* opacity: 0.8; */
-    filter: brightness(0.5);
-}
-.card:hover .back-img-empty{
-    display: block;
-}
-
-.card:hover .card-body{
-    display:block;
-}
+@import '@/assets/scss/style.scss';
 </style>
